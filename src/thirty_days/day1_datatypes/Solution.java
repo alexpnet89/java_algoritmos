9667542fe9b1478e1d4ae50980ee6c434476568e
Alex Pereira <alexpnet89@gmail.com>
2017-06-25 22:04:30 -0300

    day 1

---
+++ src/thirty_days/day1_datatypes/Solution.java
@@ -0,0 +1,27 @@
+package thirty_days.day1_datatypes;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main (String[] args){
+
+        int i = 4;
+        Double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        int i2 = scan.nextInt();
+        double d2 = scan.nextDouble();
+        scan.nextLine();
+        String  s2 = scan.nextLine();
+
+        System.out.println(i+i2);
+        System.out.println(d+d2);
+        System.out.println(s+s2);
+
+        scan.close();
+
+    }
+}
