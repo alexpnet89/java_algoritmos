d3fd6f1a5762b682ebaf7097f70beefa5f4e73d4
Alex Pereira <alexpnet89@gmail.com>
2017-06-26 20:47:23 -0300

    warmup - staircase

---
+++ src/com/warmup/staircase/Solution.java
@@ -0,0 +1,29 @@
+package com.warmup.staircase;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int contador = n;
+
+        while(contador > 0){
+            for(int j = 0;j < n; j++){
+
+                if(j+1 >= contador){
+                    System.out.printf("%s","#");
+                }else{
+                    System.out.printf("%s"," ");
+                }
+
+            }
+            System.out.printf("%s%n","");
+            contador --;
+        }
+
+
+    }
+
+}
