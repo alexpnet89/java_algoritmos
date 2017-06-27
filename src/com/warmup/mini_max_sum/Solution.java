933ffd287e4424b15e7e0db2f7ba1dd2f4dab992
Alex Pereira <alexpnet89@gmail.com>
2017-06-27 09:26:14 -0300

    day 3 - intro to conditional statmentes

---
+++ src/com/warmup/mini_max_sum/Solution.java
@@ -0,0 +1,21 @@
+package com.warmup.mini_max_sum;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        int[] soma = new int[5];
+
+        for(int i=0; i < 5; i++){
+            arr[i] = in.nextInt();
+        }
+
+    }
+}
