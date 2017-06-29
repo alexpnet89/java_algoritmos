fe8ee4f9f593ca521a273430c30484544c537398
Alex Pereira <alexpnet89@gmail.com>
2017-06-28 22:49:05 -0300

    day 4 - class vs instance

---
+++ src/thirty_days/day4_class_vs_instance/Person.java
@@ -0,0 +1,35 @@
+package thirty_days.day4_class_vs_instance;
+
+public class Person {
+    private int age;
+
+    public Person(int initialAge) {
+        // Add some more code to run some checks on initialAge
+        if (initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+            this.age = 0;
+        } else {
+            this.age = initialAge;
+        }
+    }
+
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct statement:
+        String result;
+
+        if (this.age < 13) {
+            result = "You are young.";
+        } else if (this.age >= 13 && this.age < 18) {
+            result = "You are a teenager.";
+        } else {
+            result = "You are old.";
+        }
+
+        System.out.println(result);
+    }
+
+    public void yearPasses() {
+        // Increment this person's age.
+        this.age++;
+    }
+}
